40bbf6ac43eddc7c68acc28ff777eb04d14b8e0a
luisgarces07 <fernandoluisgarces@gmail.com>
2025-06-21 16:18:05 -0300

    teste-commit/primeiro-commit

---
+++ src/main/java/com/anion/apianion/ApianionApplication.java
@@ -0,0 +1,13 @@
+package com.anion.apianion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApianionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApianionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anion/apianion/ApianionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anion.apianion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApianionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
